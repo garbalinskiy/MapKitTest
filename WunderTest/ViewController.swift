40999809351149c2b7891c908932be0578dcfa6d
Sergey Garbalinskiy <garbalinskiy@me.com>
2018-09-19 16:38:18 +0300

    Initial Commit

---
+++ WunderTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WunderTest
+//
+//  Created by Sergey Garbalinskiy on 9/19/18.
+//  Copyright © 2018 Sergey Garbalinskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
